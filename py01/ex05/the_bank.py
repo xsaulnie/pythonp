a06d379c60808c0f5bf878a7939f820b1fbfb795
Xavier SAULNIER <xsaulnie@e1r1p9.clusters.42paris.fr>
2023-02-21 17:58:17 +0100

    correction 01

--- py01/ex05/the_bank.py
+++ py01/ex05/the_bank.py
@@ -1,3 +1,5 @@
+import sys
+
 class Account(object):
     ID_COUNT = 1
     def __init__(self, name, **kwargs):
@@ -54,6 +56,7 @@ class Bank(object):
                 return False
 
         self.accounts.append(new_account)
+        print("added")
         return True
 
     def transfer(self, origin, dest, amount):
@@ -151,28 +154,47 @@ class Bank(object):
         return (True)
 
 if (__name__ == "__main__"):
+
     bank = Bank()
-    bank.add(Account(
-        'Smith Jane',
-        zip='911-745',
-        value=1000.0,
-        ref='1044618427ff2782f0bbece0abd05f31'
-    ))
-    bank.add(Account(
-        'William John',
-        zip='100-064',
-        value=6460.0,
-        ref='58ba2b9954cd278eda8a84147ca73c87',
-        info=None
-    ))
+    john = Account(
+    'William John',
+    zip='100-064',
+    brother="heyhey",
+    value=6460.0,
+    ref='58ba2b9954cd278eda8a84147ca73c87',
+    info=None,
+    other='This is the vice president of the corporation',
+    lol = "hihi"
+    )
+    bank.add(john)
+    ret = bank.fix_account("William John")
+    print(dir(john))
 
-    if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
-        print('Failed')
 
-        bank.fix_account('William John')
-        bank.fix_account('Smith Jane')
+    print(ret)
 
-    if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
-        print('Failed')
-    else:
-        print('Success')
+    # bank = Bank()
+    # bank.add(Account(
+    #     'Smith Jane',
+    #     zip='911-745',
+    #     value=1000.0,
+    #     ref='1044618427ff2782f0bbece0abd05f31'
+    # ))
+    # bank.add(Account(
+    #     'William John',
+    #     zip='100-064',
+    #     value=6460.0,
+    #     ref='58ba2b9954cd278eda8a84147ca73c87',
+    #     info=None
+    # ))
+
+    # if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
+    #     print('Failed')
+
+    #     bank.fix_account('William John')
+    #     bank.fix_account('Smith Jane')
+
+    # if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
+    #     print('Failed')
+    # else:
+    #     print('Success')
