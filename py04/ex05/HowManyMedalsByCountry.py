0f87678be0c3124870a7d4d122ec0870d4b08ef0
Xavier SAULNIER <xsaulnie@e1r8p21.clusters.42paris.fr>
2023-03-03 16:41:32 +0100

    py04 -2

--- py04/ex05/HowManyMedalsByCountry.py
+++ py04/ex05/HowManyMedalsByCountry.py
@@ -12,8 +12,52 @@ def how_many_medals_by_country(df, name):
     years = df.loc[df["Team"] == name, ["Year"]].drop_duplicates(subset=["Year"]).to_numpy()
     #self.info = df.loc[:, ["Year", "City"]].drop_duplicates(subset=["Year"])
     for y in years:
-        ret[y[0]] = {'G' : '0', 'S' : '0', 'B' : '0'}
-    print(ret)
+        ret[y[0]] = {'G' : 0, 'S' : 0, 'B' : 0}
+
+    def to_medal(m):
+        if m == "Gold":
+            return 'G'
+        elif m == "Silver":
+            return 'S'
+        elif m == "Bronze":
+            return 'B'
+
+    medals = df.loc[(df["Team"] == name) , ["Year", "Medal", "Sport"]]
+
+    #aff = [x for x in medals.to_numpy() if x[1] != 'nan']
+    #print (aff)
+    print(medals.to_numpy())
+
+    earned_team_sport = []
+
+    for m in medals.to_numpy():
+        if (m[2] in team_sports):
+            if (m[2] in earned_team_sport):
+                continue
+            else:
+                earned_team_sport.append(m[2])
+        if (m[1] == "Gold"):
+            ret[m[0]]['G'] = ret[m[0]]['G'] + 1
+        elif (m[1] == "Silver"):
+            ret[m[0]]['S'] = ret[m[0]]['S'] + 1
+        elif (m[1] == "Bronze"):
+            ret[m[0]]['B'] = ret[m[0]]['B'] + 1
+    
+    print(earned_team_sport)
+
+    rmlist = []
+    for itkey in ret.keys():
+        count = 0
+        for me, nb in ret[itkey].items():
+            count = count + nb
+        if count == 0:
+            rmlist.append(itkey)
+
+    for torm in rmlist:
+        ret.pop(torm)
+
+    #print(ret)
+    return ret
 
 
 if __name__ == "__main__":
@@ -23,4 +67,5 @@ if __name__ == "__main__":
     if data is None:
         print("Error loading data")
         sys.exit()
-    how_many_medals_by_country(data, 'France')
+    ret = how_many_medals_by_country(data, 'France')
+    print(ret[1924])
